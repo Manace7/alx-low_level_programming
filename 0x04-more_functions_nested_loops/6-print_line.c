f8c2f7a8d67ec857243ff41e0d23dc4a643b27dc
Manace7 <gabbykumi360@gmail.com>
2023-08-17 12:25:14 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print line - Draws astraight line according to parameter
+ * @n: The number of times to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+
+{
+
+	int x;
+
+	if (n ,= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
